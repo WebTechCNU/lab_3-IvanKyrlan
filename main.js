7ff824eec133a5bf395aef4c2e3738247b8130f6
ivankyrlan <kyrlan.ivan@chnu.edu.ua>
2023-04-19 08:26:21 +0300

    Merge pull request #4 from WebTechCNU/sorting sorting and range added

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const images = [
 		src: './Photos/Photo-1.jpeg',
 		alt: 'Товар 1',
 		description: 'Кофта чоловіча',
-		price: '390 ₴',
+		price: '390 грн',
 		classname: 'male'
 	},
 
@@ -11,7 +11,7 @@ const images = [
 		src: './Photos/Photo-2.jpeg',
 		alt: 'Товар 2',
 		description: 'Куртка жіноча',
-		price: '490 ₴',
+		price: '490 грн',
 		classname: 'female'
 	},
 
@@ -19,7 +19,7 @@ const images = [
 		src: './Photos/Photo-3.jpeg',
 		alt: 'Товар 3',
 		description: 'Куртка чоловіча',
-		price: '790 ₴',
+		price: '790 грн',
 		classname: 'male'
 	},
 
@@ -27,7 +27,7 @@ const images = [
 		src: './Photos/Photo-4.jpeg',
 		alt: 'Товар 4',
 		description: 'Штани чоловічі',
-		price: '690 ₴',
+		price: '690 грн',
 		classname: 'male'
 	},
 
@@ -35,7 +35,7 @@ const images = [
 		src: './Photos/Photo-5.jpeg',
 		alt: 'Товар 5',
 		description: 'Джинси жіночі',
-		price: '350 ₴',
+		price: '350 грн',
 		classname: 'female'
 	},
 
@@ -43,7 +43,7 @@ const images = [
 		src: './Photos/Photo-6.jpeg',
 		alt: 'Товар 6',
 		description: 'Кеди чоловічі',
-		price: '990 ₴',
+		price: '990 грн',
 		classname: 'male'
 	},
 
@@ -51,7 +51,7 @@ const images = [
 		src: './Photos/Photo-7.jpeg',
 		alt: 'Товар 7',
 		description: 'Джинсовка жіноча',
-		price: '550 ₴',
+		price: '550 грн',
 		classname: 'female'
 	},
 
@@ -59,7 +59,7 @@ const images = [
 		src: './Photos/Photo-8.jpeg',
 		alt: 'Товар 8',
 		description: 'Кросівки жіночі',
-		price: '1990 ₴',
+		price: '1990 грн',
 		classname: 'female'
 	}
 ];
@@ -70,10 +70,12 @@ const imagesContainer = document.getElementById('images-container');
 const showImagesBtn = document.getElementById('show-images-btn');
 const bannerImg = document.getElementById('banner-image');
 
+// Ціновий діапазон товарів
 let rangeInput = document.querySelector('#range');
 let sortedArray = images.sort(sortPrice);
-let maxPrice = Number(sortedArray[sortedArray.length - 1].price.split(' ')[0]) + 100;
-rangeInput.min = Number(sortedArray[0].price.split(' ')[0]);
+let maxPrice = Number(sortedArray[sortedArray.length - 1].price.split(' ')[0]) + 1;
+let minPrice = Number(sortedArray[0].price.split(' ')[0]) + 1;
+rangeInput.min = minPrice;
 rangeInput.max = maxPrice;
 rangeInput.defaultValue = maxPrice;
 
@@ -127,16 +129,19 @@ function addImageToContainer(image) {
 	button.addEventListener('click', function () {
 		cart.classList.add('active');
 	});
+
 }
 
 const filterButtons = document.getElementById('filter-btn');
 const footer = document.getElementById('footer');
 const goodsBtn = document.getElementById('goodsBtn');
+let newsContainer = document.getElementById('news-container');
 
 showImagesBtn.addEventListener('click', function () {
 	showImagesBtn.classList.add('d-none');
 	bannerImg.classList.add('d-none');
 	footer.classList.remove('d-none');
+	newsContainer.classList.add('d-none');
 
 	filterButtons.classList.remove('d-none');
 	images.forEach(function (image) {
@@ -148,6 +153,7 @@ goodsBtn.addEventListener('click', function () {
 	showImagesBtn.classList.add('d-none');
 	bannerImg.classList.add('d-none');
 	footer.classList.remove('d-none');
+	newsContainer.classList.add('d-none');
 
 	filterButtons.classList.remove('d-none');
 	images.forEach(function (image) {
@@ -176,7 +182,7 @@ let timerMessage = document.getElementById('timer-message')
 let timerElement = document.getElementById('timer');
 let seconds = 15;
 
-//Кнопка Уверх
+// Кнопка Уверх
 const btnUp = document.getElementById('btnUp');
 
 // Вікно з рекламою
@@ -207,7 +213,7 @@ function scrollFunction() {
 		btnUp.classList.add('d-none');
 	}
 
-	//Плавна анімація переходу догори
+	// Плавна анімація переходу догори
 	btnUp.addEventListener('click', function () {
 		window.scrollTo({
 			top: 0,
@@ -232,26 +238,7 @@ document.querySelector('div.filter-btn').addEventListener('click', (event) => {
 	});
 });
 
-//Кнопки для фільтрів
-let buttons = document.querySelectorAll('.btn-outline-secondary');
-
-function handleClick(event) {
-	let target = event.target;
-
-	if (!target.classList.contains('active')) {
-		buttons.forEach(function (button) {
-			button.classList.remove('active');
-		});
-
-		target.classList.add('active');
-	}
-}
-
-buttons.forEach(function (button) {
-	button.addEventListener('click', handleClick);
-});
-
-//Кошик
+// Кошик
 let cartIcon = document.querySelector('#cart-icon');
 let cart = document.querySelector('.cart');
 let closeCart = document.querySelector('#close-cart');
@@ -278,13 +265,13 @@ function ready() {
 		button.addEventListener('click', removeCartItem);
 	}
 
-	//Зміна кількості товару
+	// Зміна кількості товару
 	var quantityInputs = document.getElementsByClassName('cart-quantity');
 	for (var i = 0; i < quantityInputs.length; i++) {
 		var input = quantityInputs[i];
 		input.addEventListener('change', quantityChanged);
 	}
-	//Додавання товару в кошик
+	// Додавання товару в кошик
 	var addCart = document.getElementsByClassName('add-cart');
 	for (var i = 0; i < addCart.length; i++) {
 		var button = addCart[i];
@@ -293,7 +280,7 @@ function ready() {
 	// Кнопка Купити
 	document.getElementsByClassName('btn-buy')[0].addEventListener('click', buyButtonClicked);
 }
-//Кнопка Купити
+// Кнопка Купити
 function buyButtonClicked() {
 	alert('Ваше замовлення успішно оформлено');
 	let cartContent = document.getElementsByClassName('cart-content')[0]
@@ -303,14 +290,14 @@ function buyButtonClicked() {
 	updateTotal();
 }
 
-//Видалення товару з кошика
+// Видалення товару з кошика
 function removeCartItem(event) {
 	var buttonClicked = event.target;
 	buttonClicked.parentElement.remove();
 	updateTotal();
 }
 
-//Зміна кількості товару
+// Зміна кількості товару
 function quantityChanged(event) {
 	var input = event.target;
 	if (isNaN(input.value) || input.value <= 0) {
@@ -319,7 +306,7 @@ function quantityChanged(event) {
 	updateTotal();
 }
 
-//Додавання в кошик
+// Додавання в кошик
 function addCartClicked(event) {
 	var button = event.target;
 	var shopProducts = button.parentElement;
@@ -328,9 +315,6 @@ function addCartClicked(event) {
 	var productImg = shopProducts.getElementsByClassName('product-image')[0].src;
 	addProductToCart(title, price, productImg);
 	updateTotal();
-	// let cart = document.querySelector("#cart");
-	// let newElem = "<div>Item : " + title + "; price: " + price + "; </div>";
-	// cart.innerHTML += newElem;
 }
 
 function addProductToCart(title, price, productImg) {
@@ -344,7 +328,7 @@ function addProductToCart(title, price, productImg) {
 			return;
 		}
 	}
-	
+
 	let cartBoxContent = `
 					<img src="${productImg}" alt="" class="cart-img">
 					<div class="detail-box">
@@ -360,7 +344,8 @@ function addProductToCart(title, price, productImg) {
 	cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem);
 	cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('change', quantityChanged);
 }
-//Сума
+
+// Сума
 function updateTotal() {
 	var cartContent = document.getElementsByClassName('cart-content')[0]
 	var cartBoxes = cartContent.getElementsByClassName('cart-box');
@@ -369,23 +354,23 @@ function updateTotal() {
 		var cartBox = cartBoxes[i];
 		var priceElement = cartBox.getElementsByClassName('cart-price')[0]
 		var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0]
-		var price = parseFloat(priceElement.innerText.replace('₴', ''));
+		var price = parseFloat(priceElement.innerText.replace('грн', ''));
 		var quantity = quantityElement.value;
 		total = total + price * quantity;
 	}
 	total = Math.round(total * 100) / 100;
 
-	document.getElementsByClassName('total-price')[0].innerText = total + ' ₴';
+	document.getElementsByClassName('total-price')[0].innerText = total + ' грн';
 }
 
 
-// Sorting 
-function sort(attribute){
+// Сортування товарів 
+function sort(attribute) {
 	imagesContainer.innerHTML = '';
-	if(attribute == 'description'){
+	if (attribute == 'description') {
 		images.sort(sortDescription);
 	}
-	else if(attribute == 'price'){
+	else if (attribute == 'price') {
 		images.sort(sortPrice);
 	}
 	images.forEach(function (image) {
@@ -393,37 +378,136 @@ function sort(attribute){
 	});
 }
 
-function sortDescription(a,b){
-	if(a.description < b.description){
+function sortDescription(a, b) {
+	if (a.description < b.description) {
 		return -1;
-	} 
-	else if(a.description > b.description){
+	}
+	else if (a.description > b.description) {
 		return 1;
 	}
-	else{ 
+	else {
 		return 0;
 	}
 }
 
-function sortPrice(a,b){
-	if(Number(a.price.split(' ')[0]) < Number(b.price.split(' ')[0])){
+function sortPrice(a, b) {
+	if (Number(a.price.split(' ')[0]) < Number(b.price.split(' ')[0])) {
 		return -1;
-	} 
-	else if(Number(a.price.split(' ')[0]) > Number(b.price.split(' ')[0])){
+	}
+	else if (Number(a.price.split(' ')[0]) > Number(b.price.split(' ')[0])) {
 		return 1;
 	}
-	else{ 
+	else {
 		return 0;
 	}
 }
 
-function changeRange(){
+// Зміна цінового діапазону товарів
+function changeRange() {
 	let price = rangeInput.value;
 	imagesContainer.innerHTML = '';
-	document.querySelector('#text-range').innerHTML = price + ' griyvnas';
+	document.querySelector('#text-range').innerHTML = price + ' грн';
 	images.forEach(elem => {
-		if(Number(elem.price.split(' ')[0]) < price){
+		if (Number(elem.price.split(' ')[0]) < price) {
 			addImageToContainer(elem);
 		}
 	});
 }
+
+// Новини
+let newsArr = [
+	{
+		id: 1,
+		title: "Святкові образи",
+		text: "Наближаються свята, і ми готові допомогти вам створити найкращий образ на Різдво та Новий рік. У нас ви знайдете все необхідне для святкового настрою - від елегантних суконь до вечірнього одягу та аксесуарів. Оберіть свій образ для найкращих святкових фото та насолоджуйтесь святами в повному обсязі.",
+		date: new Date(2022, 11, 17, 1, 10),
+		img: '../Photos/News-1.jpeg'
+
+	},
+
+	{
+		id: 2,
+		title: "Нові поставки взуття",
+		text: "Перевірте нові моделі та виберіть ті, які підійдуть саме вам. З нашим взуттям ви будете виглядати стильно і відчувати себе комфортно.",
+		date: new Date(2023, 1, 5, 1, 10),
+		img: '../Photos/News-2.jpeg'
+	},
+
+	{
+		id: 3,
+		title: "Розпродаж зимового одягу",
+		text: "Знижки до -50% на зимову колекцію - це відмінна нагода поповнити свій гардероб якісним і теплим одягом від провідних брендів. Зробіть замовлення вже сьогодні та економте на покупках.",
+		date: new Date(2023, 2, 1, 1, 10),
+		img: '../Photos/News-3.jpeg'
+	},
+
+	{
+		id: 4,
+		title: "Нова колекція весна-літо 2023",
+		text: "Дізнайтеся про найяскравіші тенденції сезону, додайте до свого гардеробу нові луки, які підкреслять вашу унікальність.",
+		date: new Date(2023, 3, 12, 1, 10),
+		img: '../Photos/News-4.jpeg'
+	}
+]
+
+function sortNews(a, b) {
+	if (a.date < b.date) {
+		return -1;
+	}
+	if (a.date > b.date) {
+		return 1;
+	}
+	return 0;
+}
+
+function loadToCenter(id) {
+	let news = newsArr.find(elem => elem.id == id);
+	let mainContainer = document.querySelector("#news-item");
+	mainContainer.innerHTML = "<h3>" + news.title + "</h3> <p>" + news.date.toDateString() + "</p> <img src = " + news.img + " class='w-75 m-3 rounded'><p class='m-3'>" + news.text + "</p>"
+}
+
+let newsIndex = 0;
+let intervalID;
+
+function loadNews() {
+	let newsSorted = newsArr.sort(sortNews);
+	let newsContainer = document.querySelector("#news-list");
+	let todayDate = new Date();
+	let currentMonth = todayDate.getMonth();
+	let out = "";
+	newsSorted.forEach((elem) => {
+		if (elem.date.getMonth() == currentMonth) {
+			out +=
+				'<a href=#news-item onclick=loadToCenter(' +
+				elem.id +
+				') class="list-group-item list-group-item-action bg-secondary border-light active"> <div class="d - flex w - 100 justify - content - between"> <h5>' +
+				elem.title +
+				"</h5> <small>" +
+				elem.date.toDateString() +
+				"</small> </div> </a>";
+		} else {
+			out +=
+				'<a href=#news-item onclick=loadToCenter(' +
+				elem.id +
+				') class="list-group-item list-group-item-action"> <div class="d - flex w - 100 justify - content - between"> <h5>' +
+				elem.title +
+				"</h5> <small>" +
+				elem.date.toDateString() +
+				"</small> </div> </a>";
+		}
+	});
+	newsContainer.innerHTML = out;
+
+	newsIndex = newsSorted.length - 1;
+	loadToCenter(newsSorted[newsIndex].id);
+	intervalID = setInterval(changeNews, 10000);
+}
+
+function changeNews() {
+	let newsSorted = newsArr.sort(sortNews);
+	let newsCount = newsSorted.length;
+	newsIndex = (newsIndex + 1) % newsCount;
+	loadToCenter(newsSorted[newsIndex].id);
+}
+
+loadNews();
